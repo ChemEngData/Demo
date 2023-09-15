ba8e46dc6b30d1f8b194f8a4d4ec6c96ad3b1ef2
ChemEngData <chem.eng.data@proton.me>
2023-09-15 14:38:52 +0200

    I added a unit test to the code

--- basicmaths.py
+++ basicmaths.py
@@ -1,5 +1,16 @@
+import unittest
+
 def add(a,b):
     return a+b
 
 
 print(add(1,2))
+
+class TestMyAddFunc(unittest.TestCase):
+    def test_add(self):
+        answer = add(10, 20)
+        self.assertEqual(answer, 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
